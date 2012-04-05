e0559c71d75a01c1924e51406bcf2a0dc06f07ce
Micah Ernst <micahwave@gmail.com>
2012-04-04 20:40:07 -0400

    init

---
+++ dummy-posts.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Plugin Name: Dummy Posts
+ * Author: Micah Ernst
+ * Description: Adds some dummy posts to your site picking a random category and random image from media library
+ * Version: 1.0
+ */
+ 
+function dp_admin_menu() {
+	add_submenu_page( 'tools.php', 'Dummy Posts', 'Dummy Posts', 'manage_options', 'dummy-posts', 'dp_submenu_page' );
+}
+add_action( 'admin_menu', 'dp_admin_menu' );
+
+function dp_submenu_page() {
+
+	if( isset( $_POST['submit'] ) ) {
+		dp();
+	}
+
+	?>
+	<div class="wrap">
+		<h2>Dummy Posts</h2>
+		
+		<p></p>
+		
+		<form action="<?php echo admin_url( 'tools.php?page=dummy-posts' ); ?>" method="post">
+			<input type="submit" name="submit" value="Generate Dummy Posts" class="button-primary"/>
+		</form>
+	</div>
+	<?php
+}
+
+function dp() {
+
+	$max = 20;
+
+	$images = get_posts( array(
+		'post_type' => 'attachment',
+		'posts_per_page' => -1
+	));
+	
+	$cats = get_categories( array(
+		'exclude' => 1,
+		'hide_empty' => 0 
+	));
+	
+	for( $i = 1; $i <= $max; $i++ ) {
+		$text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit egestas nibh, id venenatis nisi aliquet ut. Nunc tincidunt euismod magna in iaculis. Aliquam at dapibus risus. Donec cursus sodales ipsum, ac faucibus ante adipiscing sit amet. Phasellus felis nisi, egestas interdum fermentum nec, cursus at augue. Sed tincidunt placerat aliquet. Nunc lacinia metus ac mauris sodales id feugiat mi pellentesque. Vestibulum sed est at quam facilisis vestibulum.';
+		
+		$post = array(
+			'post_title' => 'Sample Post #'.$i,
+			'post_status' => 'publish',
+			'post_content' => $text,
+			'post_type' => 'post',
+		);
+		
+		if( $cats && is_array( $cats ) ) {
+			shuffle( $cats );
+			$post['post_category'] = array( $cats[0]->term_id );
+		}
+		
+		$post_id = wp_insert_post( $post );
+		
+		if( $images && is_array( $images ) ) {
+			shuffle( $images );
+			update_post_meta( $post_id, '_thumbnail_id', $images[0]->ID );
+		}
+		
+		update_post_meta( $post_id, 'dp', 1 );
+	}
+}
